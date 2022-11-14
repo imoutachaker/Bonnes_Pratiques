5b259d66b5ad73172f25fa1ccb99da5f0ddfe7fc
imoutachaker <ines.moutachaker@insee.fr>
2022-11-14 10:41:57 +0000

    corrections

--- script.R
+++ script.R
@@ -1,19 +1,42 @@
 rm(list = ls())
-setwd("/home/onyxia/formation-bonnes-pratiques-R")
 
+# REQUIRED PACKAGES ------------ 
+# INSTALLATION =====
 if (!require("ggplot2")) install.packages("ggplot2")
 if (!require("stringr")) install.packages("stringr")
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("tidyverse")) install.packages("tidyverse")
 if (!require("MASS")) install.packages("MASS")
 
-
+# CALL PACKAGES ====
 library(tidyverse)
 library(dplyr)
+library(MASS)
+library(forcats)
 
+#FONCTIONS ====
+fonction_de_stat_agregee <- function(a, b = "moyenne", ...) {
+  ignoreNA <<- !ignoreNA
+  checkvalue <- F
+  for (x in c("moyenne", "variance", "ecart-type", "sd", "ecart type")) {
+    checkvalue <- (checkvalue | b == x)
+  }
+  if (checkvalue == FALSE) stop("statistique non supportée")
+  
+  if (b == "moyenne") {
+    x <- mean(a, na.rm = ignoreNA, ...)
+  } else if (b == "ecart-type" | b == "sd" | b == "ecart type") {
+    x <- sd(b, na.rm = ignoreNA, ...)
+  } else if (a == "variance") {
+    x <- var(a, na.rm = ignoreNA, ...)
+  }
+  return(x)
+}
+
+# IMPORT DATA ------------ 
 # j'importe les données avec read_csv2 parce que c'est un csv avec des ; et que read_csv attend comme separateur des ,
 df <- readr::read_csv2(
-  "/home/onyxia/formation-bonnes-pratiques-R/individu_reg.csv",
+  "individu_reg.csv",
   col_names = c("region", "aemm", "aged", "anai", "catl", "cs1", "cs2", "cs3", "couple", "na38", "naf08", "pnai12", "sexe", "surf", "tp", "trans", "ur")
 )
 
@@ -21,12 +44,12 @@ df <- readr::read_csv2(
 colnames(df) <- df[1, ]
 df <- df[2:nrow(df), ]
 
-df2 <- df |>
-  select(c("region", "dept", "aemm", "aged", "anai", "catl", "cs1", "cs2", "cs3", "couple", "na38", "naf08", "pnai12", "sexe", "surf", "tp", "trans", "ur"))
+df2 <- df %>%
+  dplyr::select(region, dept, aemm, aged, anai, catl, cs1, cs2, cs3, couple, na38, naf08, pnai12, sexe, surf, tp, trans, ur)
 print(df2, 20)
 
-
-# combien de professions
+# DATA EXPLORATION ------------ 
+## combien de professions ====
 print("Nombre de professions :")
 print(summarise(df2, length(unique(unlist(cs3[!is.na(cs1)])))))
 print("Nombre de professions :''")
@@ -42,7 +65,7 @@ decennie_a_partir_annee <- function(ANNEE) {
     10)
 }
 
-
+## PLOTS ===============
 df2 %>%
   select(aged) %>%
   ggplot(.) +
@@ -79,6 +102,7 @@ setwd("ome/onyxia/formation-bonnes-pratiques-R/output")
 
 ggsave(p, "p.png")
 
+# DATA TRAITEMENTS -----
 # recode valeurs manquantes
 # valeursManquantes <- data.frame(colonne = c(""), NBRE = c(NA))
 # for (i in 1:length(colnames(df2))){
@@ -100,29 +124,13 @@ df2[endsWith(df2$naf08, "Z"), "naf08"] <- NA
 str(df2)
 df2[, nrow(df2) - 1] <- factor(df2[, nrow(df2) - 1])
 df2$ur <- factor(df2$ur)
-library(forcats)
+
 df2$sexe <-
   fct_recode(df2$sexe, "Homme" = "0", "Femme" = "1")
 
 # fonction de stat agregee
 ignoreNA <- T
-fonction_de_stat_agregee <- function(a, b = "moyenne", ...) {
-  ignoreNA <<- !ignoreNA
-  checkvalue <- F
-  for (x in c("moyenne", "variance", "ecart-type", "sd", "ecart type")) {
-    checkvalue <- (checkvalue | b == x)
-  }
-  if (checkvalue == FALSE) stop("statistique non supportée")
 
-  if (b == "moyenne") {
-    x <- mean(a, na.rm = ignoreNA, ...)
-  } else if (b == "ecart-type" | b == "sd" | b == "ecart type") {
-    x <- sd(b, na.rm = ignoreNA, ...)
-  } else if (a == "variance") {
-    x <- var(a, na.rm = ignoreNA, ...)
-  }
-  return(x)
-}
 fonction_de_stat_agregee(rnorm(10))
 fonction_de_stat_agregee(rnorm(10), "cart type")
 fonction_de_stat_agregee(rnorm(10), "ecart type")
@@ -136,8 +144,8 @@ fonction_de_stat_agregee(df2 %>% filter(sexe == "Femme" & couple == "2") %>% mut
 
 api_pwd <- "trotskitueleski$1917"
 
-# modelisation
-library(MASS)
+# MODELISATION ------------
+
 df3 <- df2 %>%
   select(surf, cs1, ur, couple, aged) %>%
   filter(surf != "Z")
